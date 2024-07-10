e722a0739111dc3db1fa5334d2df64eea699f18f
syedramsha <ramsha.syed464@gmail.com>
2024-07-10 14:07:36 +0200

    feat: ADD navbar

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/investment-calculator-logo.png';
+const  Header = () => {
+    return <header id="header">
+        <img src={logo} alt='logo'/>
+        <h1>React Investment Calculator</h1>
+    </header>
+}
+
+export default Header
+
